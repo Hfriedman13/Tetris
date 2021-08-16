08163cea1e2bd9c70d7b7c8f647a04b2cffedbaf
Hfriedman13 <hfriedman13@gmail.com>
2021-08-16 16:03:51 -0400

    Create functions to validate tetromino, place tetromino, pause game, and also create new random tetromino of all cases. Also have the tetrominos falling downward and validating their position.

--- Tetris/Model/TetrisGameModel.swift
+++ Tetris/Model/TetrisGameModel.swift
@@ -13,13 +13,17 @@ class TetrisGameModel: ObservableObject {
     @Published var gameBoard: [[TetrisGameBlock?]]
     @Published var tetromino: Tetromino?
     
+    var timer: Timer?
+    var speed: Double
+    
     init(numRows: Int = 23, numColumns: Int = 10) {
         self.numRows = numRows
         self.numColumns = numColumns
         
         //allowing access to gameboard -> gameBoard[][] columns / rows respectivly
         gameBoard = Array(repeating: Array(repeating: nil, count: numRows), count: numColumns)
-        tetromino = Tetromino(origin: BlockLocation(row: 22, column: 4), blockType: .i)
+        speed = 0.1
+        resumeGame()
     }
     
     func blockClicked(row: Int, column: Int){
@@ -29,6 +33,68 @@ class TetrisGameModel: ObservableObject {
         }else {
             gameBoard[column][row] = nil
         }
+    }
+    func resumeGame() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: speed, repeats: true, block: runEngine)
+    }
+    
+    func pauseGame() {
+        timer?.invalidate()
+    }
+    
+    func runEngine(timer: Timer) {
+        //spawn new block if needed
+        guard let currentTetromino = tetromino else {
+            print("Spawning new Tetromino")
+            tetromino = Tetromino.createNewTetromino(numRows: numRows, numColumns: numColumns)
+            if !isValidTetromino(testTetromino: tetromino!) {
+                print("Game Over!")
+                pauseGame()
+            }
+            return
+        }
+        //see about moving block down
+        let newTetromino = currentTetromino.moveBy(row: -1, column: 0)
+        if isValidTetromino(testTetromino: newTetromino) {
+            print("Moving Tetromino down")
+            tetromino = newTetromino
+            return
+        }
+        //see if need to place block
+        print("Placing tetromino")
+        placeTetromino()
+    }
+    
+    func isValidTetromino(testTetromino: Tetromino) -> Bool {
+        for block in testTetromino.blocks {
+            let row = testTetromino.origin.row + block.row
+            if row < 0 || row >= numRows {return false}
+            
+            let column = testTetromino.origin.column + block.column
+            if column < 0 || column >= numColumns {return false}
+            
+            if gameBoard[column][row] != nil {return false}
+        }
+        return true
+        
+    }
+    
+    func placeTetromino() {
+        guard let currentTetromino = tetromino else {
+            return
+        }
+        
+        for block in currentTetromino.blocks {
+            let row = currentTetromino.origin.row + block.row
+            if row < 0 || row >= numRows {continue}
+            
+            let column = currentTetromino.origin.column + block.column
+            if column < 0 || column >= numColumns {continue}
+            
+            gameBoard[column][row] = TetrisGameBlock(blockType: currentTetromino.blockType)
+        }
+        tetromino = nil
     }
 }
 
@@ -43,15 +109,53 @@ enum BlockType: CaseIterable {
 struct Tetromino {
     var origin: BlockLocation
     var blockType: BlockType
+    
     var blocks: [BlockLocation] {
-        [
-            BlockLocation(row: 0, column: -1),
-            BlockLocation(row: 0, column: 0),
-            BlockLocation(row: 0, column: 1),
-            BlockLocation(row: 0, column: 2),
-        ]
+        return Tetromino.getBlocks(blockType: blockType)
     }
     
+    func moveBy(row: Int, column: Int) -> Tetromino {
+        let newOrigin = BlockLocation(row: origin.row + row, column: origin.column + column)
+        return Tetromino(origin: newOrigin, blockType: blockType)
+    }
+    
+    static func getBlocks(blockType: BlockType) -> [BlockLocation] {
+        switch blockType {
+        case .i:
+            return [BlockLocation(row: 0, column: -1), BlockLocation(row: 0, column: 0), BlockLocation(row: 0, column: 1),
+                    BlockLocation(row: 0, column: 2) ]
+        case .o:
+            return [BlockLocation(row: 0, column: 0), BlockLocation(row: 0, column: 1), BlockLocation(row: 1, column: 1),
+                    BlockLocation(row: 1, column: 0) ]
+        case .t:
+            return [BlockLocation(row: 0, column: -1), BlockLocation(row: 0, column: 0), BlockLocation(row: 0, column: 0),
+                    BlockLocation(row: 1, column: 0) ]
+        case .j:
+            return [BlockLocation(row: 1, column: -1), BlockLocation(row: 0, column: -1), BlockLocation(row: 0, column: 0),
+                    BlockLocation(row: 0, column: 1) ]
+        case .l:
+            return [BlockLocation(row: 0, column: -1), BlockLocation(row: 0, column: 0), BlockLocation(row: 0, column: 1),
+                    BlockLocation(row: 1, column: 1) ]
+        case .s:
+            return [BlockLocation(row: 0, column: -1), BlockLocation(row: 0, column: 0), BlockLocation(row: 1, column: 0),
+                    BlockLocation(row: 1, column: 1) ]
+        case .z:
+            return [BlockLocation(row: -1, column: 0), BlockLocation(row: 0, column: 0), BlockLocation(row: 0, column: -1),
+                    BlockLocation(row: -1, column: 1) ]
+        }
+    }
+    //creating new
+    //going through all blocks and making sure none are above gameboard
+    static func createNewTetromino(numRows: Int, numColumns: Int) -> Tetromino {
+        let blockType = BlockType.allCases.randomElement()!
+        
+        var maxRow = 0
+        for block in getBlocks(blockType: blockType) {
+            maxRow = max(maxRow, block.row)
+        }
+        let origin = BlockLocation(row: numRows - 1 - maxRow, column: (numColumns-1)/2)
+        return Tetromino(origin: origin, blockType: blockType)
+    }
 }
 
 struct BlockLocation {
